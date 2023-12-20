593a2b9f1a5d84b77bfcf69e1be004a4f507f6ff
Oscar Ochoa <97921525+bustyAI@users.noreply.github.com>
2023-12-20 10:04:49 -0800

    changing route

--- constants/index.ts
+++ constants/index.ts
@@ -4,8 +4,8 @@ export const headerLinks = [
     route: "/",
   },
   {
-    label: "Create Event",
-    route: "/events/create",
+    label: "Create Van",
+    route: "/vans/create",
   },
   {
     label: "My Profile",
