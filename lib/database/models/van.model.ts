bee1442b112d044e4fd2e953ed4d13fe856b28ca
Oscar Ochoa <97921525+bustyAI@users.noreply.github.com>
2023-12-20 10:00:33 -0800

    creating van Schema

---
+++ lib/database/models/van.model.ts
@@ -0,0 +1,16 @@
+import { Schema, model, models } from "mongoose";
+
+const VanSchema = new Schema({
+  vanId: { type: String, required: true, unique: true },
+  vanType: { type: String, required: true },
+  fuelType: { type: String, required: true },
+  fuelLevel: { type: Number },
+  chargePercent: { type: Number },
+  isCharging: { type: Boolean },
+  maintenance: { type: String },
+  isCharged: { type: Boolean },
+});
+
+const Van = models.Van || model("Van", VanSchema);
+
+export default Van;
