93a8b5891321d634561c5936a2859d70a80df5e8
Oscar Ochoa <97921525+bustyAI@users.noreply.github.com>
2023-12-20 10:10:11 -0800

    creating Van Form

--- app/(root)/vans/create/page.tsx
+++ app/(root)/vans/create/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const CreateVan = () => {
-  return <div>CreateVan</div>;
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold  sm:text-left md:text-center">
+          Create Van
+        </h3>
+      </section>
+
+      <div className="wrapper my-8"></div>
+    </>
+  );
 };
 
 export default CreateVan;
