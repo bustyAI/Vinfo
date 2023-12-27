04e092497ac48dfaad8e256b454f7af42b87995c
Oscar Ochoa <97921525+bustyAI@users.noreply.github.com>
2023-12-27 05:53:42 -0800

    creating structure for displaying cards

--- components/shared/VanCollection.tsx
+++ components/shared/VanCollection.tsx
@@ -1,5 +1,6 @@
 import { IVan } from "@/lib/database/models/van.model";
 import React from "react";
+import Card from "./Card";
 
 type CollectionProps = {
   data: IVan[];
@@ -21,7 +22,29 @@ const VanCollection = ({
   collectionType,
   urlParamName,
 }: CollectionProps) => {
-  return <div>VanCollection</div>;
+  return (
+    <>
+      {data.length > 0 ? (
+        <div className="flex flex-col items-center gap-10">
+          <ul className="grid w-full grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:gap-10">
+            {data.map((van) => {
+              const hasOrderLink = collectionType === "Vans_Organized";
+              return (
+                <li key={van._id} className="flex justify-center">
+                  <Card />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      ) : (
+        <div className="flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center">
+          <h3 className="p-bold-20 md:h-5-bold ">{emptyTitle}</h3>
+          <p className="p-regular-14">{emptyStateSubtext}</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default VanCollection;
